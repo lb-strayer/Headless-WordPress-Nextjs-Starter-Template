8b9d068bbd4dcd59f9a90ca32114735ea0fe7859
lb-strayer <lecrisha.bryan@my.strayer.edu>
2023-04-23 13:20:52 -0500

    modified components/Footer/index.tsx with personalized info

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -6,20 +6,20 @@ export const Footer = () => {
       <div className="text-center border-t py-8">
         <h3 className="text-xl">Important Links</h3>
         <div className="flex items-center justify-center my-4 underline">
-          <Link href="https://dev.to/jeffsalive" className="px-4">
-            Dev.to (Jeffrey)
+          <Link href="https://dev.to/lpbryan" className="px-4">
+            Dev.to (Lecrisha)
           </Link>
-          <Link href="https://twitter.com/JeffreySunny1" className="px-4">
+          <Link href="https://twitter.com/HellraiserBarbi" className="px-4">
             Twitter
           </Link>
-          <Link href="https://linkedin.com/in/jeffsalive" className="px-4">
+          <Link href="https://linkedin.com/in/Lecrisha" className="px-4">
             LinkedIn
           </Link>
-          <Link href="https://jeffreynwankwo.com" className="px-4">
+          <Link href="https://HellraiserBarbie.com" className="px-4">
             Website
           </Link>
         </div>
-        <small>Jeffrey &copy; {new Date().getFullYear()}</small>
+        <small>Lecrisha &copy; {new Date().getFullYear()}</small>
       </div>
     </footer>
   );
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["headless-wordpress-nextjs-starter-template.local", "api-headless-wordpress-nextjs-blog.000webhostapp.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
